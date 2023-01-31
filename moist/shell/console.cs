bdb25fb583c7d856c94b7ca4a601215b6c2393b7
Ben Daws <123220788+bensyxx@users.noreply.github.com>
2023-01-31 15:51:11 -0600

    Create console.cs

---
+++ moist/shell/console.cs
@@ -0,0 +1,17 @@
+
+
+using System;
+
+namespace Dotnet;
+  
+  public static class Program;
+    
+    public static void Main(string[] args) {
+      
+      Console.WriteText("Welcome to Xylo, the Moist shell.")
+      Console.WriteText("Version 0.1 ---------------------")
+      Console.WriteText("GitHub: https://github.com/bensyxx/moist")
+      
+    }
+  }
+}
